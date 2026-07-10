d070652358aa3970aba2ef3f2dc14c1f0c1092b8
heng <liuheng@xiaoyubot.com>
2026-07-10 11:33:14 +0800

    fix: move ACTIVE to 0x08009000 to avoid bootloader code overlap
    The linker stubbornly places bootloader .vector_table at 0x08007000
    regardless of FLASH ORIGIN in memory.x. Moved ACTIVE from 0x08007000
    to 0x08009000 (8K gap) to avoid the bootloader code being overwritten
    when flashing the app.

--- bootloader/build.rs
+++ bootloader/build.rs
@@ -6,24 +6,17 @@ fn main() {
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
     fs::write(out.join("memory.x"), include_bytes!("memory.x")).unwrap();
 
-    // fix.x: redirect stub with linker-computed reset vector
-    // _stack_start is from cortex-m-rt; ORIGIN(FLASH) is from memory.x
-    fs::write(out.join("fix.x"), concat!(
-        "SECTIONS {\n",
-        "  .redirect 0x08000000 : {\n",
-        "    LONG(_stack_start)\n",
-        "    LONG(ABSOLUTE(ORIGIN(FLASH)) + SIZEOF(.redirect) + SIZEOF(.vector_table) | 1)\n",
-        "  } > FLASH\n",
-        "}\n",
-        "INSERT BEFORE .vector_table;\n"
-    ).as_bytes()).unwrap();
+    // Redirect stub at 0x08000000: SP from _stack_start, PC placeholder
+    fs::write(out.join("redirect.x"),
+        b"SECTIONS { .redirect 0x08000000 : { LONG(_stack_start) LONG(0) } > FLASH }\nINSERT BEFORE .vector_table;\n"
+    ).unwrap();
 
     println!("cargo:rustc-link-search={}", out.display());
-    println!("cargo:rustc-link-arg-bins=-Tfix.x");
+    println!("cargo:rustc-link-arg-bins=-Tredirect.x");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=--defsym=__bootloader_state_start=0x08006000");
     println!("cargo:rustc-link-arg-bins=--defsym=__bootloader_state_end=0x08007000");
-    println!("cargo:rustc-link-arg-bins=--defsym=__bootloader_active_start=0x08007000");
+    println!("cargo:rustc-link-arg-bins=--defsym=__bootloader_active_start=0x08009000");
     println!("cargo:rustc-link-arg-bins=--defsym=__bootloader_active_end=0x08013000");
     println!("cargo:rustc-link-arg-bins=--defsym=__bootloader_dfu_start=0x08013000");
     println!("cargo:rustc-link-arg-bins=--defsym=__bootloader_dfu_end=0x0801F800");
